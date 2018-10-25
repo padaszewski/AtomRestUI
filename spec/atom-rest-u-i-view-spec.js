62ba659bdb64ba499349b8d5e4a129bf96e53099
Sebastian Schmidt <schmidtseb@outlook.de>
2018-10-25 12:54:53 +0200

    Inital commit

---
+++ spec/atom-rest-u-i-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRestUIView from '../lib/atom-rest-u-i-view';
+
+describe('AtomRestUIView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
